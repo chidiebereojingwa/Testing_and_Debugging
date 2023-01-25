d74db17755c36d8ec31699c198755f5d46af3240
chidiebereojingwa <ojingwaoji@gmail.com>
2023-01-25 13:07:12 +0100

    made changes

--- src/components/listings/Grid.test.js
+++ src/components/listings/Grid.test.js
@@ -4,7 +4,7 @@ import data from "../../data/courses.json"
 const numData = data.length
 
 test('Number of items = 12', () => {
-    expect(numData).toBe(11)    
+    expect(numData).toBe(12)    
 })
 
 test('Number of items to be greater than or equal to 12', () => {
