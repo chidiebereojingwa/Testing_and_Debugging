e7e25c3144c9c4ffef44a9c2221e2cc500e54356
chidiebereojingwa <ojingwaoji@gmail.com>
2023-01-26 14:17:01 +0100

    optimized code

--- src/components/listings/Grid.test.js
+++ src/components/listings/Grid.test.js
@@ -3,25 +3,33 @@ import data from "../../data/courses.json"
 
 const numData = data.length
 
-test('Number of items = 12', () => {
-    expect(numData).toBe(12)    
+
+describe('Number test', () => { 
+    test('Number of items = 12', () => {
+        expect(numData).toBe(12)
+    })
+
+    test('Number of items to be greater than or equal to 12', () => {
+        expect(numData).toBeGreaterThanOrEqual(12);
+    })
 })
 
-test('Number of items to be greater than or equal to 12', () => {
-    expect(numData).toBeGreaterThanOrEqual(12); 
-})
+
 
 
 const matchData = data[0].title
 
-test('There is JS in this title', () => { 
-    expect(matchData).toMatch(/JS/);
-})
+describe('Title test', () => { 
+    test('There is JS in this title', () => {
+        expect(matchData).toMatch(/JS/);
+    })
 
-test('The title contains React', () => { 
-    expect(matchData).toContain("React");
-})
+    test('The title contains React', () => {
+        expect(matchData).toContain("React");
+    })
 
+
+})
 
 // Arrays
 
