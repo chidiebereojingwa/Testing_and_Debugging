6aecb44a4c088024cf7d28a472246fad91aa2b01
chidiebereojingwa <ojingwaoji@gmail.com>
2023-01-26 13:42:09 +0100

    Added test to app.test

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -3,6 +3,13 @@ import renderer from 'react-test-renderer';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+
+test('App snapshot test', () => { 
+  const component = renderer.create(<App />);
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+}) 
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
